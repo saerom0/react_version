ba133d27fe0e378db04c6c476315a1ee2013cf0e
saerom0 <jinxinsr@naver.com>
2023-03-14 13:24:45 +0900

    gallery 컴포넌트 반응형 완성

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -9,13 +9,13 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function Gallery() {
 	const dispatch = useDispatch();
-	const my_id = '197108414@N08';
+	const myId = '197108414@N08';
 	const masonryOptions = { transitionDuration: '0.5s' };
 	const frame = useRef(null);
 	const input = useRef(null);
 	const modal = useRef(null);
-	const [Loading, setLoading] = useState(true);
-	const [Index, setIndex] = useState(0);
+	const [loading, setLoading] = useState(true);
+	const [index, setIndex] = useState(0);
 
 	const Items = useSelector((store) => store.flickr.data);
 
@@ -35,7 +35,7 @@ function Gallery() {
 	};
 
 	const showMine = () => {
-		dispatch(fetchFlickr({ type: 'user', user: my_id }));
+		dispatch(fetchFlickr({ type: 'user', user: myId }));
 		frame.current.classList.remove('on');
 		setLoading(true);
 	};
@@ -51,93 +51,95 @@ function Gallery() {
 	return (
 		<>
 			<Layout name={'Gallery'}>
-				<h1>GALLERY</h1>
-				<div id='gallery'>
-					<h2 onClick={showMine}>SIGHTSEEING</h2>
-					<div className='search_box'>
-						<button className='interest-btn' onClick={showInterest}>
-							<FontAwesomeIcon className='arrow' icon={faArrowRight} />
-							TO OTHER PICTURES
-						</button>
-						<input
-							type='text'
-							ref={input}
-							placeholder='검색어 입력'
-							onKeyUp={(e) => e.key === 'Enter' && showSearch()}
-						/>
-						<button className='search-btn' onClick={showSearch}>
-							Search
-						</button>
+				<div className='container'>
+					<h1>GALLERY</h1>
+					<div id='gallery'>
+						<h2 onClick={showMine}>SIGHTSEEING</h2>
+						<div className='searchBox'>
+							<button className='interestBtn' onClick={showInterest}>
+								<FontAwesomeIcon className='arrow' icon={faArrowRight} />
+								TO OTHER PICTURES
+							</button>
+							<input
+								type='text'
+								ref={input}
+								placeholder='검색어 입력'
+								onKeyUp={(e) => e.key === 'Enter' && showSearch()}
+							/>
+							<button className='searchBtn' onClick={showSearch}>
+								Search
+							</button>
+						</div>
+
+						<section className='galleryTitle'>
+							<p>
+								All you need for business success. Stay inspired in a shared
+								environment with like-minded individuals pursuing their passions
+								and become part of a creative community that stretches
+							</p>
+							<p>
+								Lower your costs and save time with our business concierge,
+								professional and technical services.
+							</p>
+							<p>
+								We provide on-demand services from translation to company
+								registration so you are free to fully focus on your business.
+							</p>
+						</section>
 					</div>
 
-					<section className='gallery-title'>
-						<p>
-							All you need for business success. Stay inspired in a shared
-							environment with like-minded individuals pursuing their passions
-							and become part of a creative community that stretches
-						</p>
-						<p>
-							Lower your costs and save time with our business concierge,
-							professional and technical services.
-						</p>
-						<p>
-							We provide on-demand services from translation to company
-							registration so you are free to fully focus on your business.
-						</p>
-					</section>
-				</div>
+					{loading && (
+						<img
+							className='loading'
+							src={`${process.env.PUBLIC_URL}/img/loading.gif`}
+							alt='로딩이미지'
+						/>
+					)}
 
-				{Loading && (
-					<img
-						className='loading'
-						src={`${process.env.PUBLIC_URL}/img/loading.gif`}
-						alt='로딩이미지'
-					/>
-				)}
+					<div className='frame' ref={frame}>
+						<Masonry elementType={'div'} options={masonryOptions}>
+							{Items.map((item, idx) => {
+								return (
+									<article key={idx}>
+										<div className='inner'>
+											<div
+												className='pic'
+												onClick={() => {
+													setIndex(idx);
+													modal.current.open();
+												}}
+											>
+												<img
+													src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+													alt={item.title}
+												/>
+											</div>
+											<span>{item.title}</span>
 
-				<div className='frame' ref={frame}>
-					<Masonry elementType={'div'} options={masonryOptions}>
-						{Items.map((item, idx) => {
-							return (
-								<article key={idx}>
-									<div className='inner'>
-										<div
-											className='pic'
-											onClick={() => {
-												setIndex(idx);
-												modal.current.open();
-											}}
-										>
 											<img
-												src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
-												alt={item.title}
+												className='profile'
+												src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+												alt={item.owner}
+												onError={(e) =>
+													e.target.setAttribute(
+														'src',
+														'https://www.flickr.com/images/buddyicon.gif'
+													)
+												}
 											/>
 										</div>
-										<span>{item.title}</span>
-
-										<img
-											className='profile'
-											src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
-											alt={item.owner}
-											onError={(e) =>
-												e.target.setAttribute(
-													'src',
-													'https://www.flickr.com/images/buddyicon.gif'
-												)
-											}
-										/>
-									</div>
-								</article>
-							);
-						})}
-					</Masonry>
+									</article>
+								);
+							})}
+						</Masonry>
+					</div>
 				</div>
 			</Layout>
 
 			<Modal ref={modal}>
 				<img
-					src={`https://live.staticflickr.com/${Items[Index]?.server}/${Items[Index]?.id}_${Items[Index]?.secret}_b.jpg`}
-					alt={Items[Index]?.title}
+					src={`https://live.staticflickr.com/${Items[index]?.server}/${Items[index]?.id}_${Items[index]?.secret}_b.jpg`}
+					alt={Items[index]?.title}
 				/>
 			</Modal>
 		</>
